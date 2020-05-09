5ba26651423c6fe057ba62ddcff6fdfa7bdfe1e3
keyhoh <iheyhoh+git@gmail.com>
2020-05-09 22:30:07 +0900

    add: idとpasswordによる認証を実装

--- src/main/java/tk/keyhoh/domain/account/AccountService.java
+++ src/main/java/tk/keyhoh/domain/account/AccountService.java
@@ -1,7 +1,19 @@
 package tk.keyhoh.domain.account;
 
 public class AccountService {
-    public Account createAccount() {
-        return new Account();
+    private final IPassword iPassword;
+
+    AccountService(IPassword iPassword){
+        this.iPassword = iPassword;
+    }
+
+    public boolean authenticate(Id id, Password password) {
+        return iPassword.exists(id, password);
+    }
+
+    public Account createAccount(String password) {
+        var account = new Account();
+        iPassword.save(account.id, new Password(password));
+        return account;
     }
 }
